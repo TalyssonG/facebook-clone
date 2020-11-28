23cdc2ea9212d1d55a906248ca9c504b3be9dd72
TalyssonG <lopestalysson99@gmail.com>
2020-11-28 15:08:38 -0300

    AppBar

---
+++ components/AppBar.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+
+import styled from 'styled-components/native'
+import { Feather, MaterialCommunityIcons} from '@expo/vector-icons'
+
+const container = styled.View`
+ width: 100%;
+ height: 60px;
+ padding: 0 11px;
+ align-items: center;
+ flex-direction: row;
+ justify-content: space-between;
+
+`
+const Text = styled.Text`
+ color: #3a86e9;
+ font-size: 25px;
+ font-weight: bold;
+ letter-spacing: -0.3px;
+`
+const Row = styled.View `
+  flex-direction:row;
+`
+const Button = styled.TouchableOpacity`
+ width: 42px;
+ height: 42px;
+ border-radius: 21px;
+ background: #eeeeee;
+ align-items: center;
+ justify-content: center;
+ margin-left: 16px;
+ `
+
+const AppBar = () => {
+    return (
+  <Container>
+      <Text>
+          Facebook
+      </Text>
+
+      <Row>
+          <Button>
+              <Feather
+                  name="Search"
+                  size={29}
+                  color="#000"
+                  />  
+              
+          </Button>
+
+          <Button>
+              <MaterialCommunityIcons
+                 name="facebook-messenger"
+                 size={29}
+              />
+          </Button>
+      </Row>
+  </Container>
+
+    )
+}
+
+export default AppBar
+
